8e9dc3966cb387b31891dd7d649a3815d044cad5
RajeshPatkar <patkar@rajeshpatkar.com>
2024-07-25 01:06:59 +0530

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::io::{self, Write};
+
+/// Moves the cursor to the specified (x, y) coordinates in the terminal.
+pub fn teleport_to(x: u32, y: u32) {
+    print!("\x1B[{};{}H", y, x);
+    io::stdout().flush().unwrap();
+}
+
+/// Prints a single character to the terminal and immediately flushes the output.
+pub fn putchar(ch: char) {
+    print!("{}", ch);
+    io::stdout().flush().unwrap();
+}
+
+/// Clears the entire screen and moves the cursor to position (1, 1).
+pub fn clear_screen() {
+    print!("\x1B[2J");
+    teleport_to(1, 1);
+    io::stdout().flush().unwrap();
+}
